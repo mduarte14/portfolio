12442123070b93507b51ef93ebcf831d9dab0524
Miguel Duarte <duartemiguel04@gmail.com>
2024-06-08 14:50:14 -0700

    Fixed footer issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,13 +39,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Image
-                src={info.img}
-                alt="icons"
-                width={20}
-                height={20}
-                ref={socialMedia.link}
-              />
+              <Image src={info.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
